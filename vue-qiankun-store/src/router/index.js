b1a8a411a5f04e333d00a62f2a5d5877b2a22bd1
hetao <18681562686@163.com>
2024-04-30 10:34:33 +0800

    修改子应用项目

---
+++ vue-qiankun-store/src/router/index.js
@@ -0,0 +1,35 @@
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+// Vue.use(VueRouter)
+
+// 判断环境是否是微应用打开
+let microPath = ''
+if (window.__POWERED_BY_QIANKUN__) {
+  microPath = '/store'
+}
+
+
+const routes = [
+  {
+    path: microPath + '/',
+    redirect: microPath + '/homepage'
+  },
+  {
+    path: microPath + '/homepage',
+    name: 'homepage',
+    component: () => import(/* webpackChunkName: "homepage" */ '../views/Homepage.vue')
+  },
+  {
+    path: microPath + '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+// const router = new VueRouter({
+//   routes
+// })
+
+export default routes
